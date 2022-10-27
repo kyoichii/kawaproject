bc3db43c1e2ce1a3cd412f6289a6b5f96e928afc
Kyoichi <kyoichi.807@gmail.com>
2022-10-27 12:08:30 +0900

    初回準備

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+//フロント画面へ行くための処理(ここに画面遷移の処理を書いてください)
+function buttonClick(number) {
+    switch (number) {
+        case 1:
+            alert("レジ画面1クリック！");
+            //画面遷移方法。
+            // window.location.href = 'パス名'; // 通常の遷移
+            // window.open('パス名', '_blank'); // 新しいタブを開き、ページを表示
+            break;
+        case 2:
+            alert("レジ画面2クリック！");
+            break;
+        case 3:
+            alert("レジ会計画面1クリック！");
+            break;
+        case 4:
+            alert("レジ会計画面2クリック！");
+            break;
+        default:
+            alert("エラー！");
+    }
+}
