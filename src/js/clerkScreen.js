aa45631fe823f7cc713b602454ed11aa074d61f0
HamamotoHinata <57205423+HamamotoHinata@users.noreply.github.com>
2022-11-07 16:37:47 +0900

    画面のデザインと簡易的な動的処理を実装しました

---
+++ src/js/clerkScreen.js
@@ -0,0 +1,19 @@
+// たまねぎ
+function onion() {
+    document.getElementById("box").innerText = "たまねぎ";
+}
+
+// にんじん
+function carrot() {
+    document.getElementById("box").innerText = "にんじん";
+}
+
+// 豚肉 
+function pork() {
+    document.getElementById("box").innerText = "豚肉";
+}
+
+// 購入確定
+function total() {
+    document.getElementById("box").innerText = "購入確定";
+}
