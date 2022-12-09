5aa645f376789e949ac0a9d60649b081b4a305ae
HamamotoHinata <57205423+HamamotoHinata@users.noreply.github.com>
2022-12-09 11:12:03 +0900

    a

--- source/js/clerkscreen.js
+++ source/js/clerkscreen.js
@@ -103,9 +103,7 @@ function minusclick(vegenumber) {
 //合計表示関数
 function totalarea() {
     document.getElementById('quantity').innerHTML = `合計:${totalcount}個`;
-    document.getElementById('displayQuantity').innerHTML = `個数:${totalcount}個`;
     document.getElementById('price').innerHTML = `会計:${totalprice}円`;
-    document.getElementById('displayPrice').innerHTML = `会計:${totalprice}円`;
 }
 
 /*
